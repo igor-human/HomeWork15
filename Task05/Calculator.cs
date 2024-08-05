62fcc625cf6c5b99c97746c6343c454959dc118b
Igor Korzhan <igor.human@gmail.com>
2024-08-05 22:00:09 +0300

    Task05

---
+++ Task05/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task05
+{
+    internal class Calculator
+    {
+        // Метод для сложения
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        // Метод для вычитания
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+
+        // Метод для умножения
+        public double Mul(double a, double b)
+        {
+            return a * b;
+        }
+
+        // Метод для деления с проверкой на деление на ноль
+        public double Div(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Ошибка: деление на ноль.");
+            }
+            return a / b;
+        }
+    }
+}
---
+++ Task05/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using Task05;
+
+namespace WorketExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            try
+            {
+                Console.Write("Введите первое число: ");
+                double num1 = double.Parse(Console.ReadLine());
+
+                Console.Write("Введите второе число: ");
+                double num2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Выберите операцию: +, -, *, /");
+                char operation = Console.ReadLine()[0];
+
+                double result = 0;
+
+                switch (operation)
+                {
+                    case '+':
+                        result = calculator.Add(num1, num2);
+                        break;
+                    case '-':
+                        result = calculator.Sub(num1, num2);
+                        break;
+                    case '*':
+                        result = calculator.Mul(num1, num2);
+                        break;
+                    case '/':
+                        result = calculator.Div(num1, num2);
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка: неверная операция.");
+                        return;
+                }
+
+                Console.WriteLine($"Результат: {result}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: введите корректное число.");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+
+
+        }
+
+
+    }
+
+
+
+}
